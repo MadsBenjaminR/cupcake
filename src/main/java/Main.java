8d1daad1821723d92edded257267e55bbc53e468
Mads Benjamin Ribberholt <benjamin.ribberholt@gmail.com>
2024-04-03 10:16:43 +0200

    mapper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("hej");
 
-        System.out.println("hej med dig");
-        System.out.println("hej lars fra molda");
-
-        System.out.println("Mac vil altid være bedre");
-
-        System.out.println("hej med dig");
     }
 }
