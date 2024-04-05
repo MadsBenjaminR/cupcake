e22b314a8926348a9473f14aacb7f5c7dc9ed8a0
MINGO-INC <143798588+MINGO-INC@users.noreply.github.com>
2024-04-05 11:33:35 +0200

    .
    cartlineupdate

--- src/main/java/app/controllers/CartLineController.java
+++ src/main/java/app/controllers/CartLineController.java
@@ -1,19 +1,16 @@
 package app.controllers;
 
-import app.entities.Bottom;
-import app.entities.Cart;
-import app.entities.CartLine;
-import app.entities.Top;
+import app.entities.*;
 import app.exceptions.DatabaseException;
 import app.persistence.CreateACupcakeMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
-import kotlin.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
-public class CreateACupcake {
+
+public class CartLineController {
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)
     {
@@ -42,7 +39,7 @@ public class CreateACupcake {
     }
 
     public static void orderLineSum(Context ctx, ConnectionPool connectionPool) {
-
+        User user = ctx.sessionAttribute("currentUser");
         int sum=0;
         int totalitems=0;
 
@@ -53,26 +50,23 @@ public class CreateACupcake {
         }
         try {
 
-
     int toppingId = Integer.parseInt(ctx.formParam("top"));
     int bottomId = Integer.parseInt(ctx.formParam("bottom"));
     int quantity = Integer.parseInt(ctx.formParam("quantity"));
 
     Top top = CreateACupcakeMapper.getToppingById(toppingId, connectionPool);//laver et objet af top
     Bottom bottom = CreateACupcakeMapper.getBaseById(bottomId, connectionPool);//laver et objet af base
-
+ List<CartLine> units=cart.getCartLines();
 
 
     cart.add(top, bottom, quantity);
+    sum = cart.getTotal();
 
-    sum= cart.getTotal();
 
     ctx.sessionAttribute("cart", cart.getCartLines());
-    ctx.sessionAttribute("sum",sum);
-    totalitems=cart.totalItemsInCart();
-    ctx.sessionAttribute("totalitems",totalitems);
-
-
+    ctx.sessionAttribute("sum", sum);
+    totalitems = cart.totalItemsInCart();
+    ctx.sessionAttribute("totalitems", totalitems);
 
 
     ctx.render("/cart.html");
@@ -83,5 +77,21 @@ public class CreateACupcake {
 
     }
 
+    public static void insertInhistory(Context ctx, ConnectionPool connectionPool){
+
+        Cart cart = ctx.sessionAttribute("cart");
+        if(cart==null){
+            cart =new Cart();
+
+        }
+//        List<CartLine> alllines=cart.getCartLines();
+
+       // int pricePrUnit=alllines.getFirst().getTop().getPrice()+alllines.getLast().getBottom().getPrice();
+
+//        for (int i=0 ; i <=alllines.size() ; i++){
+           // OrderlineMapper.inSertOrderHistory(pricePrUnit,user,sum,connectionPool);
+
+        }
+    }
+
     
-}
--- src/main/java/app/persistence/OrderlineMapper.java
+++ src/main/java/app/persistence/OrderlineMapper.java
@@ -1,6 +1,8 @@
 package app.persistence;
 
+import app.entities.Bottom;
 import app.entities.Orderline;
+import app.entities.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class OrderlineMapper {
 
@@ -40,5 +43,45 @@ public class OrderlineMapper {
             throw new RuntimeException(e);
         }
         return orderlines;
+    }
+    public static void deductFromCart(int price, User user, int sum,ConnectionPool connectionPool) {
+
+        String sql = "SELECT * from users where user_id=?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql);
+        ) {
+            ps.setInt(1, user.getUserId());
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                int currentPrice = rs.getInt("price");
+                int newTotalPrice = currentPrice - price;
+                if (newTotalPrice < sum) {
+                    System.out.println("Insufficient funds!");
+
+                }
+                  String sql02 = "update users set balance=? where user_id=?";
+                PreparedStatement ps02 = connection.prepareStatement(sql02);
+                    ps02.setInt(1, newTotalPrice);
+                    ps02.setInt(2, user.getUserId());
+                    ps02.executeQuery();
+                    System.out.println("Balance has been updated");
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+    public static void inSertOrderHistory(int pricePrUnit, User user, int sum, ConnectionPool connectionPool) {
+
+
+
     }
 }
